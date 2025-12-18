fc258cd7dfd06f65db7d35608b00ca61095740ae
Xuaan <nic110397@gmail.com>
2025-12-19 00:51:08 +0800

    [ MEJAA ] + Refactor code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3209,8 +3209,9 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   double _calculateAdditionalCost(Map<String, dynamic> item) {
     if (item['additional_cost'] != null) {
       return (item['additional_cost'] as num).toDouble();
+    } else {
+      return _calculateVariantCost(item['custom_variant_info']);
     }
-    return _calculateVariantCost(item['custom_variant_info']);
   }
 
   double _calculateVariantCost(dynamic variantInfo) {
